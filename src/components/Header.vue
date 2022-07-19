a742c05df8910792273e055b5daf13a83862d0fb
Crisano97 <97keb6@gmail.com>
2022-07-19 15:25:22 +0200

    header structure

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+        <img src="../assets/logo-small.svg" alt="Spotify logo">
+  </header>
+</template>
+
+<script>
+import Header from '../../../vue-dc-comics/src/components/Header.vue'
+export default {
+  components: { Header },
+
+}
+</script>
+
+<style lang="scss" scoped>
+    header{
+        // height: 5vh;
+        padding: 1rem;
+        background-color: #2E3A46;
+
+        img{
+            width: 2.5rem;
+        }
+    }
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+    <main>
+        <h1>main</h1>
+    </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    main{
+        height: 95vh;
+        background-color: #1E2D3B;
+    }
+</style>
